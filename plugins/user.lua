c9ba938829fb945bd0bee15d466bd14d5b4cef05
Saumyajyoti Mukherjee <saumyajyoti@gmail.com>
2023-06-05 17:32:05 +0530

    Update user.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,39 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+   { "rcarriga/nvim-notify", init = false, config = true },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astronvim.utils").list_insert_unique(
+          opts.ensure_installed,
+          { "bash", "markdown", "markdown_inline", "regex", "vim" }
+        )
+      end
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- cmdline = { view = "cmdline" },
+      messages = { view_search = false },
+      lsp = {
+        progress = { enabled = false },
+        hover = { enabled = false },
+        signature = { enabled = false },
+      },
+      routes = {
+        { filter = { event = "msg_show", find = "%d+L,%s%d+B" }, opts = { skip = true } }, -- skip save notifications
+        { filter = { event = "msg_show", find = "^%d+ more lines$" }, opts = { skip = true } }, -- skip paste notifications
+        { filter = { event = "msg_show", find = "^%d+ fewer lines$" }, opts = { skip = true } }, -- skip delete notifications
+        { filter = { event = "msg_show", find = "^%d+ lines yanked$" }, opts = { skip = true } }, -- skip yank notifications
+      },
+      presets = {
+        long_message_to_split = true,
+        command_palette = true,
+      },
+    },
+  },
 }
